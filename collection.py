faf442bba178146a803e3fc4e58bc0086ca0d7a4
ahmadx17 <kathafy4hmad@gmail.com>
2026-06-10 18:18:43 +0530

    funtions.py

--- collection.py
+++ collection.py
@@ -1,7 +1,10 @@
 #collection--list,tuple,set,dictionaries
-#  -list[]= > allows duplicate,can store any data type,can modifiable
+#  -list[]=> allows duplicate,can store any data type,can modifiable
 #            list.append(),list.insert(),list.pop(),list.extend()
-#  -tuple()= > allows duplicate,can store any data type,but can't modify,tuple=list()
+#  -tuple()=> allows duplicate,can store any data type,but can't modify,tuple=list()
+#  -set{}=> 
+#          add(),update(),pop()
+
 
 
 a=(1,2,3,4,5,6,7)
---
+++ functions.py
@@ -0,0 +1,11 @@
+def add():
+    print(5+5)
+def sub():
+    print(5-5)
+def mul():
+    print(5*5)
+def dev():
+    print(5/5)
+
+sub()    
+
